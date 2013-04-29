9f5e1d7b4fcb28f20bd666325a2a9243cbf494b7
Matthew Weier O'Phinney <matthew@zend.com>
2013-04-29 15:00:32 -0500

    zendframework/zf2@ce6d90055c0854d6a150e1c6b27da813fcc1efeb (1367262807)

--- Hydrator/ClassMethods.php
+++ Hydrator/ClassMethods.php
@@ -15,10 +15,10 @@ use Zend\Stdlib\Exception;
 use Zend\Stdlib\ArrayUtils;
 use Zend\Stdlib\Hydrator\Filter\FilterComposite;
 use Zend\Stdlib\Hydrator\Filter\FilterProviderInterface;
-use Zend\Stdlib\Hydrator\Filter\MethodMatchFilter;
 use Zend\Stdlib\Hydrator\Filter\GetFilter;
 use Zend\Stdlib\Hydrator\Filter\HasFilter;
 use Zend\Stdlib\Hydrator\Filter\IsFilter;
+use Zend\Stdlib\Hydrator\Filter\MethodMatchFilter;
 use Zend\Stdlib\Hydrator\Filter\NumberOfParameterFilter;
 
 class ClassMethods extends AbstractHydrator implements HydratorOptionsInterface
@@ -136,7 +136,7 @@ class ClassMethods extends AbstractHydrator implements HydratorOptionsInterface
             $attribute = $method;
             if (preg_match('/^get/', $method)) {
                 $attribute = substr($method, 3);
-                if(!property_exists($object, $attribute)) {
+                if (!property_exists($object, $attribute)) {
                     $attribute = lcfirst($attribute);
                 }
             }
